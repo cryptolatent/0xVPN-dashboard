458ff577c57c51a40752da61f2c1cbe1c0230a70
cryptolatent <cryptolatent@proton.me>
2024-04-12 02:44:06 -0500

    Release version for Desktop Client v0.1

--- assets/index-Dyt1yLVP.js
+++ assets/index-Dyt1yLVP.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-CnrDx6Lm.js";import{g as Ur}from"./index-CNH7nKTc.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-B4O_sy8M.js";import{g as Ur}from"./index-0H4mFD3i.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
