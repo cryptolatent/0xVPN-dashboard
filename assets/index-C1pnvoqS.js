71af7f529e391866629c4f6eb9101031f75662a0
cryptolatent <cryptolatent@proton.me>
2024-04-12 23:31:14 -0500

    Change history mode to hash

--- assets/index-C1pnvoqS.js
+++ assets/index-C1pnvoqS.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-ALtYFwni.js";import{g as Ur}from"./index-BtlTW2ms.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-CbOrpYq-.js";import{g as Ur}from"./index-RWfVPzIR.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
